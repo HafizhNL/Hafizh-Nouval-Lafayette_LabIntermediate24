8b65f276d3a369ad19305e98b399794478577938
HafizhNL <hafizh.nouval25@gmail.com>
2025-01-18 09:09:59 +0700

    percobaan 1

--- Tugas1_Hangman_Hafizh.py
+++ Tugas1_Hangman_Hafizh.py
@@ -4,9 +4,14 @@ long_word = len(secret_word)
 for i in range(long_word):
     guess = input("Enter a letter: ")
     if len(guess) > 1:
-        print("Your guess is too high")
+        print("Your guess is too high, Try Again!")
         break
     if guess.isalpha() == False:
-        print("Must be a letter")
+        print("Must be a letter, Try Again!")
         break
+    if guess == secret_word[i]:
+        print("Correct!")
+    else:
+        print("Incorrect!")
         
+
